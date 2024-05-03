b0dc6b1f52098ed0b5cffc26cfe0f1e379d74730
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-05-03 22:27:07 +0400

    UserMapper interface done

--- emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/mapper/UserMapper.java
+++ emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package az.edu.ada.wm2.empcrudsecurity.model.mapper;
+import az.edu.ada.wm2.empcrudsecurity.model.dto.UserDto;
+import az.edu.ada.wm2.empcrudsecurity.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    List<UserDto> userListToUserDtoList(List<User> user);
+
+    UserDto userToUserDto(User user);
+
+
 }
