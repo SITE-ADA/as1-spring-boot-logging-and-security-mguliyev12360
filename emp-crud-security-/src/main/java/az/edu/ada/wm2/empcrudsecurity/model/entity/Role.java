f796f34910e06c179a1b2c3c74d38dc465dd063f
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-05-03 22:25:19 +0400

    entity classes done

--- emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/entity/Role.java
+++ emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/entity/Role.java
@@ -1,4 +1,23 @@
 package az.edu.ada.wm2.empcrudsecurity.model.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+    private static Long nextId = 1L;
+    private Long id;
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+        this.id = nextId++;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/entity/User.java
+++ emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/entity/User.java
@@ -1,4 +1,77 @@
 package az.edu.ada.wm2.empcrudsecurity.model.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String roles;
+
+    @Transient
+    private List<Role> rolesList = new ArrayList<>(Arrays.asList(new Role("ROLE_USER"), new Role("ROLE_ADMIN")));
+    @Transient
+    private List<String> authorities =  new ArrayList<>(Arrays.asList("ROLE_USER"));
+
+    public List<GrantedAuthority> getAuthorities() {
+        return authorities.stream().map(role -> new SimpleGrantedAuthority(role)).collect(Collectors.toList());
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User addRole(String authority) {
+        this.authorities.add(authority);
+        return this;
+    }
+
+    @PrePersist
+    @PreUpdate
+    private void saveRoles() {this.roles = String.join(";", this.authorities);}
+
+    @PostLoad
+    private void readRoles() {this.authorities = Arrays.stream(this.roles.split(";")).collect(Collectors.toList());}
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
