b324672cbcec9f5071385cbe9e4a4b48f7b597c2
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-05-03 18:25:37 +0400

    dto classes are done

--- emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/dto/UserDto.java
+++ emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/dto/UserDto.java
@@ -1,4 +1,20 @@
 package az.edu.ada.wm2.empcrudsecurity.model.dto;
 
+import az.edu.ada.wm2.empcrudsecurity.model.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+    private Long id;
+    private String username;
+    private String roles;
+    private List<Role> rolesList;
+
+
 }
--- emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/entity/Employee.java
+++ emp-crud-security-/src/main/java/az/edu/ada/wm2/empcrudsecurity/model/entity/Employee.java
@@ -1,4 +1,48 @@
 package az.edu.ada.wm2.empcrudsecurity.model.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 50)
+    private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 50)
+    private String lastName;
+
+    @NotNull
+    private Double salary;
+
+    public Employee(@NotNull String firstName, @NotNull String lastName, @NotNull Double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
